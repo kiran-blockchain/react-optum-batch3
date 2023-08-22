9c8270346bc22828b3c6415041b5f8aca609ec2e
BRUGUSOFT <kiran.paturi@brugu.io>
2023-08-22 16:03:24 +0530

    added code

--- flipkart/src/pages/Login.jsx
+++ flipkart/src/pages/Login.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { registerConfig } from "../config/registerConfig"
+import { useNavigate } from "react-router-dom";
+import { Textbox } from "../components/Textbox";
 export const Login = () => {
+    const [login,setLogin] = useState({   
+        email:"",
+        password:"",
+    });
+    const handleChange =(element)=>{
+        const updatedValue = login;
+        updatedValue[element.name]= element.value;
+        setLogin({...updatedValue});
+    };
+    const navigate = useNavigate();
     return (
-        <h1>I am login</h1>
+        <form className="container mt-5">
+
+            <Textbox textBoxConfig={registerConfig.email} 
+            changeEvt={handleChange} />
+            <Textbox textBoxConfig={registerConfig.password}
+             changeEvt={handleChange} />
+            <div class="col-md-3">
+                <button className="btn btn-success" onClick={e=>{
+                    navigate("/products");
+                }}>Login</button>
+            </div>
+        </form>
     )
 }
